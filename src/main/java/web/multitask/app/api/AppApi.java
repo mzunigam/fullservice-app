3cd183add2eed6160aab392a23d5c218e8e1dbfc
Paolo <mzunigam@ucvvirtual.edu.pe>
2023-10-09 22:30:56 -0500

    INITIAL COMMIT

---
+++ src/main/java/web/multitask/app/api/AppApi.java
@@ -0,0 +1,39 @@
+package web.multitask.app.api;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.*;
+import web.multitask.app.mysql.ProcedureMysql;
+
+@RestController
+@CrossOrigin("*")
+@RequestMapping("/api")
+public class AppApi {
+
+    final ProcedureMysql procedureMysql;
+
+    @Autowired
+    public AppApi(ProcedureMysql procedureMysql) {
+        this.procedureMysql = procedureMysql;
+    }
+
+    @PostMapping("/procedure")
+    public String callProcedure(@RequestBody String body) {
+        JSONObject json = new JSONObject(body);
+
+        if (json.has("procedure")) {
+            try {
+                JSONArray params = json.isNull("params") ? new JSONArray() : json.getJSONArray("params");
+                JSONObject response = procedureMysql.ProcedureExecution(json.getString("procedure"), params.toList().toArray());
+                return response.put("status", true).put("message", "Success").toString();
+            } catch (Exception e) {
+                return new JSONObject().put("data", new JSONArray()).put("message", e.getMessage()).put("status", false).toString();
+            }
+        } else {
+            return new JSONObject().put("data", new JSONArray()).put("message", "Invalid Request").put("status", false).toString();
+        }
+    }
+
+}
---
+++ src/main/java/web/multitask/app/mysql/ProcedureMysql.java
@@ -0,0 +1,32 @@
+package web.multitask.app.mysql;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@Service
+public class ProcedureMysql {
+    private final JdbcTemplate jdbcTemplate;
+    @Autowired
+    public ProcedureMysql(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    public JSONObject ProcedureExecution(String query , Object[] params) {
+        try {
+            List<Map<String, Object>> list = jdbcTemplate.queryForList(query, params);
+            JSONObject result = new JSONObject();
+            result.put("data", list);
+            return result;
+        } catch (Exception e) {
+            Logger.getLogger("ProcedureMysql").warning(e.getMessage());
+        }
+        return null;
+    }
+}
